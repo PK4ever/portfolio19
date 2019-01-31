019ff9951a6f25d4bf51a3b85a3b8c345b405430
pk4ever <paulkwoyelo@rocketmail.com>
2019-01-31 09:28:21 -0500

    started projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,9 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
+  projects = [
+    new Project('Kitechen Assist', 'http://kitchenassist.me/',
+      'https://github.com/kitchen-assistant', 'A smart Kitchen Assistant that utilizes Google action flow and Firebase ' +
+      '. Received an honorable mention from over 4,500 participants from 124 countries', ['angularfire', 'firebase']),
+    new Project('140 CLINTON AVE', 'http://cs.oswego.edu/~pkwoyelo/realEstateListing/view/',
+      '', 'A freelance project for a real-estate company', []),
+    new Project( '', 'http://kitchenassist.me/', 'https://github.com/kitchen-assistant',
+      'A smart Kitchen Assistant that utilizes Google action flow and Firebase ' +
+      '. Received an honorable mention from over 4,500 participants from 124 countries', []),
+  ];
   constructor() { }
   ngOnInit() {
   }
 
 }
+export class Project {
+  constructor(public name: String, public weblink: String,
+              public gitUrl: String, public description: String, public techList: String[] ) {}
+}
+
+

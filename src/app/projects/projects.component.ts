c92101e6588018d06381d212429bd30730698036
pk4ever <paulkwoyelo@rocketmail.com>
2019-02-01 16:47:16 -0500

    done?

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,23 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   projects = [
-    new Project('Kitechen Assist', 'http://kitchenassist.me/',
-      'https://github.com/kitchen-assistant', 'A smart Kitchen Assistant that utilizes Google action flow and Firebase ' +
-      '. Received an honorable mention from over 4,500 participants from 124 countries', ['angularfire', 'firebase']),
-    new Project('140 CLINTON AVE', 'http://cs.oswego.edu/~pkwoyelo/realEstateListing/view/',
-      '', 'A freelance project for a real-estate company', []),
-    new Project( '', 'http://kitchenassist.me/', 'https://github.com/kitchen-assistant',
+    new Project('Kitechen Assist',
+      'kitchenAssist.png',
+      ['angular / angularfire', 'javascript', 'firebase'],
       'A smart Kitchen Assistant that utilizes Google action flow and Firebase ' +
-      '. Received an honorable mention from over 4,500 participants from 124 countries', []),
+     '. Received an honorable mention from over 4,500 participants from 124 countries',
+      'https://github.com/kitchen-assistant',
+      'http://kitchenassist.me/'),
+    new Project(
+      '140 CLINTON AVE',
+      '140ClintonAve.png',
+      ['html5', 'css3', 'javascript'],
+       'A freelanced web page for a real-estate company',
+      'https://github.com/PK4ever/realEstateListings',
+      'http://cs.oswego.edu/~pkwoyelo/realEstateListing/view/'),
+    new Project( 'Style Rush',
+      'styleRush.png',
+      ['android', 'firebase', 'swift'],
+      'App Coming soon!'),
+    new Project( 'LakerPolling',
+      'lakerPolling.png',
+      ['java-spring', 'javascript', 'css', 'Am-Charts'],
+      'An open source clicker software that enables for in class and online polling and class participation.',
+      'https://github.com/PK4ever/laker-polling'),
+    new Project( 'OZEvents',
+      'msslHome.png',
+      ['java-spark', 'postgresql'],
+      'The very first full-stack application that I built while experimenting with different java web frameworks. ',
+      'https://github.com/PK4ever/mssl', 'https://mssl.herokuapp.com/'),
   ];
   constructor() { }
   ngOnInit() {
   }
-
 }
 export class Project {
-  constructor(public name: String, public weblink: String,
-              public gitUrl: String, public description: String, public techList: String[] ) {}
+  constructor(public name: String, public imageUrlName: String, public techList: String[],
+              public description?: String, public gitUrl?: String, public weblink?: String) {}
 }
 
 
